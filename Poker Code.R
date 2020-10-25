c7bf87acb8db26ecc05ab0296ffeeda97dad9101
whgonzalez <57110438+wonhagonzalez@users.noreply.github.com>
2020-10-24 22:16:56 -0500

    Original R Code

--- Poker Code.R
+++ Poker Code.R
@@ -1,29 +1,34 @@
 Friday_Poker <- read.csv("C:/Users/wonha/Desktop/My Projects/Friday_Poker.csv")
+Friday_Poker$Poker_Wagered <- Friday_Poker$Buy.ins*25
 Friday_Poker$Poker_Earnings <- Friday_Poker$Poker_Winnings - Friday_Poker$Poker_Wagered 
 Friday_Poker$Side_WinPct <- Friday_Poker$Side_Bets_Won/Friday_Poker$Side_Bets
 Friday_Poker$Total_Action <- Friday_Poker$Buy.ins + Friday_Poker$Side_Bets
 Friday_Poker$Total_Earnings <- Friday_Poker$Poker_Earnings + Friday_Poker$Side_Earnings
+Friday_Poker$Wager_Efficiency <- round(Friday_Poker$Total_Earnings/Friday_Poker$Total_Action, 2)
+Friday_Poker <- Friday_Poker[, c(1, 2, 8, 3, 4, 9, 5:7, 10:13)]
 
 ##dealing with NaN for win pct.
 sum(is.na(Friday_Poker$Side_WinPct))
 Friday_Poker$Side_WinPct[is.na(Friday_Poker$Side_WinPct)] <- NA
 sum(is.na(Friday_Poker$Side_WinPct))
 
+## Create data set with active players
+active.play <- Friday_Poker[which(Friday_Poker$Total_Action > 0), ]
+
 library(ggplot2)
 library(plotly)
 
-p <- ggplot(Friday_Poker) + aes(x = Total_Earnings, y = Total_Action) + geom_point()
+p <- ggplot(active.play) + aes(x = Total_Earnings, y = Total_Action) + geom_point()
 ggplotly(p)
 
-p1 <- ggplot(Friday_Poker) +
- aes(x = Total_Action, y = Total_Earnings, colour = Participants) +
- geom_point(size = 3L) +
- scale_color_hue() +
- labs(x = "# of Bets", y = "Earnings ($)", title = "Friday Poker", subtitle = "Total Earnings vs. Total Bets Made", color = "Players") +
+p1 <- ggplot(active.play) + 
+ aes(x = Total_Action, y = Total_Earnings, colour = Participants, size = Wager_Efficiency) +
+ geom_point() + scale_color_hue() + 
+ labs(x = "# of Bets", y = "Earnings ($)", title = "Friday Poker", subtitle = "Total Earnings vs. Total Bets Made", color = "Players", size = "Wager Efficiency") +
  theme_minimal()
 ggplotly(p1)
 
-p2 <- ggplot(Friday_Poker) +
+p2 <- ggplot(active.play) +
  aes(x = Buy.ins, y = Victories, colour = Participants, size = Poker_Winnings) +
  geom_point() +
  scale_color_hue() +
@@ -31,7 +36,7 @@ p2 <- ggplot(Friday_Poker) +
  theme_minimal()
 ggplotly(p2)
 
-p3 <- ggplot(Friday_Poker) +
+p3 <- ggplot(active.play) +
  aes(x = Side_Bets, y = Side_Earnings, colour = Participants, size = Side_WinPct) +
  geom_point() +
  scale_color_hue() +
@@ -39,3 +44,5 @@ p3 <- ggplot(Friday_Poker) +
  theme_minimal()
 ggplotly(p3)
 
+#Write new csv
+write.csv(Friday_Poker, "C:/Users/wonha/Desktop/My Projects/Friday Poker Dataset.csv")
